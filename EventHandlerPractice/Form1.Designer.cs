5727ed17d3fec220a1ec8c35cf309ccee88d5158
Jihye Yang <s2127@e-mirim.hs.kr>
2023-05-31 12:08:56 +0900

    버튼 2 이벤트 핸들러 연결

--- EventHandlerPractice/Form1.Designer.cs
+++ EventHandlerPractice/Form1.Designer.cs
@@ -6,7 +6,6 @@
         /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
         /// <summary>
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
@@ -19,9 +18,7 @@
             }
             base.Dispose(disposing);
         }
-
         #region Windows Form 디자이너에서 생성한 코드
-
         /// <summary>
         /// 디자이너 지원에 필요한 메서드입니다. 
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
@@ -34,9 +31,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(801, 177);
+            this.button1.Size = new System.Drawing.Size(395, 189);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,30 +41,27 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(0, 183);
+            this.button2.Location = new System.Drawing.Point(12, 208);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(801, 276);
+            this.button2.Size = new System.Drawing.Size(395, 185);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(420, 409);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-
         }
-
         #endregion
-
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
 }
-
